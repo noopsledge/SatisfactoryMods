259dafc6dee00ff4ee13ea6bfa306c8ed32bc3dd
NoOp Sledge <248062093+noopsledge@users.noreply.github.com>
2025-12-20 13:24:28 +0000

    [BigLifts] Add a static multiplier onto the max lift height

--- BigLifts/Source/BigLifts/Private/BigLifts.cpp
+++ BigLifts/Source/BigLifts/Private/BigLifts.cpp
@@ -1,7 +1,19 @@
 #include "BigLifts.h"
 
+#include "FGConveyorLiftHologram.h"
+#include "Patching/NativeHookManager.h"
+
+static constexpr float MaxLiftHeightMultiplier = 10.0f;
+
 void FBigLiftsModule::StartupModule()
 {
+#if !WITH_EDITOR
+	SUBSCRIBE_UOBJECT_METHOD_AFTER(AFGConveyorLiftHologram, BeginPlay,
+		[](AFGConveyorLiftHologram* hologram)
+		{
+			hologram->mMaximumHeight *= MaxLiftHeightMultiplier;
+		});
+#endif
 }
 
 void FBigLiftsModule::ShutdownModule()
