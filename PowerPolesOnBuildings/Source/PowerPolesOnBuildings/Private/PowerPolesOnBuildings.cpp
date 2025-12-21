8f7ffcadc6b76f7db1a07f2f125debb0462d7059
NoOp Sledge <248062093+noopsledge@users.noreply.github.com>
2025-12-21 16:26:22 +0000

    [PowerPolesOnBuildings] New project

---
+++ PowerPolesOnBuildings/Source/PowerPolesOnBuildings/Private/PowerPolesOnBuildings.cpp
@@ -0,0 +1,11 @@
+#include "PowerPolesOnBuildings.h"
+
+void FPowerPolesOnBuildingsModule::StartupModule()
+{
+}
+
+void FPowerPolesOnBuildingsModule::ShutdownModule()
+{
+}
+
+IMPLEMENT_MODULE(FPowerPolesOnBuildingsModule, PowerPolesOnBuildings)
