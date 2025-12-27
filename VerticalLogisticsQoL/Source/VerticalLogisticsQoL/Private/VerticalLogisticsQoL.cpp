1ef781df38bc4a8eb2fd93b8ef069c85ed9b755e
NoOp Sledge <248062093+noopsledge@users.noreply.github.com>
2025-12-27 18:57:29 +0000

    [VerticalLogisticsQoL] New project

---
+++ VerticalLogisticsQoL/Source/VerticalLogisticsQoL/Private/VerticalLogisticsQoL.cpp
@@ -0,0 +1,11 @@
+#include "VerticalLogisticsQoL.h"
+
+void FVerticalLogisticsQoLModule::StartupModule()
+{
+}
+
+void FVerticalLogisticsQoLModule::ShutdownModule()
+{
+}
+
+IMPLEMENT_MODULE(FVerticalLogisticsQoLModule, VerticalLogisticsQoL)
